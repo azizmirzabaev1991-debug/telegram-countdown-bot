1896fc0ecee38b887f62fa2a522e5c6b41220197
azizmirzabaev1991-debug <azizmirzabaev1991@gmail.com>
2026-06-30 18:41:02 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,26 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
+import asyncio
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.environ["BOT_TOKEN"]
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "⏳ Assalomu alaykum!\nCountdown bot ishga tushdi."
     )
 
-app = Application.builder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+async def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+    print("Bot ishga tushdi...")
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+    while True:
+        await asyncio.sleep(3600)
+
+if __name__ == "__main__":
+    asyncio.run(main())
